57440f010f33a8394098ce7d512fd78ecdcd13a7
Ali Ahmadov <alishka.ahmadovv@gmail.com>
2026-02-11 15:46:28 +0400

    Add GitHub Actions deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,12 +5,17 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   app: {
+    baseURL: '/designer_portfolio/',
     pageTransition: {
       name: 'leave-up',
       mode: 'default', // NOT out-in
     },
   },
   css: ['~/assets/css/main.css', '~/assets/css/fonts.css'],
+  ssr: true,
+  nitro: {
+    preset: 'static'
+  },
   vite: {
     plugins: [
       tailwindcss(),
